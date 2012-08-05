45adfebf943861d8fb22bae0a1735b09d0b1aa68
christophe <meumeu1402@gmail.com>
2012-08-05 15:58:41 +0000

    Auth lib

--- api.js
+++ api.js
@@ -5,6 +5,14 @@ function version(req, res, next){
   res.send('Api version : 0.1');
 }
 
+function authenticate(req, res, next){
+	console.log(req.path);
+	console.log(req.query);
+	console.log(req.params);
+	
+
+}
+
 function respond(req, res, next) {
 	var module = lib['auth'];
 	var obj = new module();
@@ -12,14 +20,25 @@ function respond(req, res, next) {
 }
 
 var server = restify.createServer();
+server.use(restify.queryParser({ mapParams: true }));
+
+// # Routes
+
+// ## Api version
 server.get('/', version);
 server.head('/', version);
 server.get('/api', version);
 server.head('/api', version);
 
+// ## Authentication
+server.get('/auth/:name', authenticate);
+server.head('/auth/:name', authenticate);
 
-server.get('/hello/:name', respond);
-server.head('/hello/:name', respond);
+// ## Hello
+server.get('/app/:app/:function', respond);
+server.head('/app/:app/:function', respond);
+
+// # Starting server
 
 server.listen(8000, function() {
   console.log('%s listening at %s', server.name, server.url);
